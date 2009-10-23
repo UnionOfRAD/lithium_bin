a8fd14752b1a57bce5419de1762b09a6be12d811
alkemann <alek.morland@gmail.com>
2009-10-23 22:39:37 +0200

    removed some unused code from latest() added documentation

--- models/Paste.php
+++ models/Paste.php
@@ -6,6 +6,17 @@ use \Geshi;
 use \lithium\util\Validator;
 use \lithium\data\Connections;
 
+/**
+ * Data model for access the pastebin documents.
+ * In this instance it works as an inbetween of the controller
+ * and the CouchDB adapter. The goal being that the controller
+ * can ask for data in a very generic way.
+ *
+ *
+ * @link	http://rad-dev.org/lithium_bin
+ * @package	lithium_bin
+ * @author	alkemann
+ */
 class Paste extends \lithium\core\Object {
 
 	/**
@@ -55,14 +66,21 @@ class Paste extends \lithium\core\Object {
 			'language' => 'javascript',
 			'views' => array(
 				'all' => array(
-					'map' => 'function(doc) { var preview = String.substring(doc.content, 0, 100); emit(doc.author, {author:doc.author, language:doc.language, preview: preview, created: doc.created}); }'
+					'map' =>
+'function(doc) {
+	var preview = String.substring(doc.content, 0, 100);
+	emit(doc.author, {
+		author:doc.author, language:doc.language, preview: preview, created: doc.created
+	});
+}'
 				)
 			)
 		)
 	);
 
 	/*
-	* Validate data
+	* Validate the input data before saving to data
+	* Validates author, content, language, permanent
 	*
 	* @return stdClass
 	*/
@@ -141,6 +159,13 @@ class Paste extends \lithium\core\Object {
 		return $data;
 	}
 
+	/**
+	 * Create a stdClass data object with default values
+	 * optionally include start values of your own.
+	 *
+	 * @param array $data of field values to start with
+	 * @return stdClass a dataobject
+	 */
 	public static function create($data = array()) {
 		$data += static::$_defaults;
 		$data['created'] = date('Y-m-d h:m:s');
@@ -173,6 +198,14 @@ class Paste extends \lithium\core\Object {
 		return $result;
 	}
 
+	/**
+	 * Find all (or a limited amount of) documents
+	 *
+	 * @param string $type search types, currently only supports 'all'
+	 * @param array $options valid keys are:
+	 *		- limit : number of documents to retrieve
+	 * @return stdClass data object
+	 */
 	public static function find($type = 'all', $options = array()) {
 		$modifiers = '';
 		if (isset($options['limit'])) {
@@ -190,12 +223,15 @@ class Paste extends \lithium\core\Object {
 
 		return $data;
 	}
-	
+
+	/**
+	 * Direct access to the CouchDB view called 'latest'
+	 *
+	 * @param string $type
+	 * @return stdClass object
+	 */
 	public static function latest($type = 'default') {
-		$modifiers = '';
-		if (isset($options['limit'])) {
-			$modifiers = '?limit='.$options['limit'];
-		}
+		$modifiers = '?limit=10';
 		$couch = Connections::get('couch');
 		$data = $couch->get(static::$_meta['source'].'/_design/latest/_view/all'.$modifiers);
 		
