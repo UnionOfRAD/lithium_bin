909a4c2fa1f61fc17d415d2fd2c46eb7c48ee380
alkemann <alek.morland@gmail.com>
2009-10-23 22:56:48 +0200

    added documentation to the controller. rearrange order of actions

--- controllers/PastesController.php
+++ controllers/PastesController.php
@@ -4,15 +4,69 @@ namespace app\controllers;
 
 use \app\models\Paste;
 
+/**
+ * Controller that decides what data is available to the different actions (urls)
+ * and what views that should be rendered. It does this by asking the Paste model
+ * in the most generic way possible so as to not get involved with how the data is
+ * handled.
+ *
+ * @link	http://rad-dev.org/lithium_bin
+ * @package	lithium_bin
+ * @author	alkemann
+ */
 class PastesController extends \lithium\action\Controller {
 
 	/**
-	 * Index
+	 * Asks the model for the data to be rendered at /latest
+	 * showing the 10 latest pastes made.
 	 *
 	 * @return array
 	 */
 	public function index() {
 		return array('latest' => Paste::latest('all', array('limit' => 10)));
+	}
+
+	/**
+	 * Controller action for url /view/[id]
+	 *
+	 * Verify that an id has been supplied, ask the model for it,
+	 * verify that one was found, set the language (for layout),
+	 * pass along the variables to view and layout
+	 *
+	 * @param string $id the uuid that the paste is saved under.
+	 * @return array variables to pass to layout and view
+	 */
+	public function view($id = null) {
+		$paste = Paste::findFirstById($id);
+		if ($paste == null) {
+			$this->redirect(array('controller' => 'pastes', 'action' => 'index'));
+		}
+		$binJs = true;
+		return compact('paste','binJs');
+	}
+
+	/**
+	 * Add paste
+	 *
+	 * @todo add cookie / session remembering of author name
+     *
+	 * @param string $author
+	 * @param string $language
+	 */
+	public function add($author = null, $language = null) {
+		if (empty($this->request->data)) {
+			$paste = Paste::create(compact('author', 'language'));
+		} else {
+			$paste = Paste::save($this->request->data);
+			if ($paste->saved) {
+				$this->redirect(array(
+					'controller' => 'pastes', 'action' => 'view', 'args' => array($paste->_id)
+				));
+			}
+		}
+		$languages = Paste::$languages;
+		$this->set(compact('paste', 'languages'));
+		$this->render('form');
 	}
 
 	/**
@@ -38,46 +92,6 @@ class PastesController extends \lithium\action\Controller {
 		$languages = Paste::$languages;
 		$this->set(compact('paste', 'languages'));
 		$this->render('form');
-	}
-
-	/*
-	 * Add paste
-	 *
-	 * @todo add cookie / session remembering of author name
-	 */
-	public function add($author = null, $language = null) {
-		if (empty($this->request->data)) {
-			$paste = Paste::create(compact('author', 'language'));
-		} else {
-			$paste = Paste::save($this->request->data);
-			if ($paste->saved) {
-				$this->redirect(array(
-					'controller' => 'pastes', 'action' => 'view', 'args' => array($paste->_id)
-				));
-			}
-		}
-		$languages = Paste::$languages;
-		$this->set(compact('paste', 'languages'));
-		$this->render('form');
-	}
-
-	/**
-	 * Controller action for url /view/[id]
-	 *
-	 * Verify that an id has been supplied, ask the model for it,
-	 * verify that one was found, set the language (for layout),
-	 * pass along the variables to view and layout
-	 *
-	 * @param string $id the uuid that the paste is saved under.
-	 * @return array variables to pass to layout and view
-	 */
-	public function view($id = null) {
-		$paste = Paste::findFirstById($id);
-		if ($paste == null) {
-			$this->redirect(array('controller' => 'pastes', 'action' => 'index'));
-		}
-		$binJs = true;
-		return compact('paste','binJs');
 	}
 }
 
