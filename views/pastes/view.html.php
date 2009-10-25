4fb3b892b87b87ad8b77d1f86f3870d349c30618
alkemann <alek.morland@gmail.com>
2009-10-25 23:59:58 +0100

    edit button for pastes

--- views/pastes/view.html.php
+++ views/pastes/view.html.php
@@ -10,11 +10,37 @@
 
 <div class="nav tabs light">
 	<ul>
-		<li><a class="color" href="#" id="toggle-color" title="Toggle Color"><span>Color</span></a></li>
-		<li><a class="contrast" href="#" id="toggle-contrast" title="Toggle Contrast"><span>Contrast</span></a></li>
-		<li><a class="bigger" href="#" id="code-bigger" title="Larger code"><span>Larger</span></a></li>
-		<li><a class="smaller" href="#" id="code-smaller" title="Smaller code"><span>Smaller</span></a></li>
-		<li><a class="copy" href="#" id="copy-to-clipboard" title="Copy to clipboard"><span>Copy</span></a></li>
+		<li>
+			<a class="color" href="#" id="toggle-color" title="Toggle Color">
+				<span>Color</span>
+			</a>
+		</li>
+		<li>
+			<a class="contrast" href="#" id="toggle-contrast" title="Toggle Contrast">
+				<span>Contrast</span>
+			</a>
+		</li>
+		<li>
+			<a class="bigger" href="#" id="code-bigger" title="Larger code">
+				<span>Larger</span>
+			</a>
+		</li>
+		<li>
+			<a class="smaller" href="#" id="code-smaller" title="Smaller code">
+				<span>Smaller</span>
+			</a>
+		</li>
+		<li>
+			<a class="copy" href="#" id="copy-to-clipboard" title="Copy to clipboard">
+				<span>Copy</span>
+			</a>
+		</li>
+		<li>
+			<a class="edit" href="<?=@$this->_request->env('base');?>/edit/<?=@$paste->_id;
+			?>" id="code-edit" title="Edit paste">
+				<span>Edit</span>
+			</a>
+		</li>
 	</ul>
 </div>
 
