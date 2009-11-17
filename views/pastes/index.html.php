7de0d5ebb4583406f5f2fce9636012edd314ef85
alkemann <alek.morland@gmail.com>
2009-11-17 21:22:55 +0100

    updated `index` view to be compatible with `Document`

--- views/pastes/index.html.php
+++ views/pastes/index.html.php
@@ -6,14 +6,14 @@
 		<th>Date</th>
 	</tr>
 	<tbody>
-	<?php foreach ($latest->rows as $key => $row): ?>
+	<?php foreach ($latest as $key => $row): ?>
 		<tr class="<?=($key % 2) ? 'del1' : 'del2'; ?>">
-			<td class="<?=$row->value->language; ?>" title="<?=$row->value->language; ?>">
+			<td class="<?=$row->language; ?>" title="<?=$row->language; ?>">
 				<?php echo $this->html->link(substr($row->id, 0, 12) . '...', '/view/'.$row->id); ?>
 			</td>
-			<td class="preview"><?=$row->value->preview; ?></td>
-			<td><?=$row->value->author; ?></td>
-			<td><?php echo date('Y-m-d H:i', strtotime($row->value->created)); ?></td>
+			<td class="preview"><?=$row->preview; ?></td>
+			<td><?=$row->author; ?></td>
+			<td><?php echo date('Y-m-d H:i', strtotime($row->created)); ?></td>
 		</tr>
 	<?php endforeach;?>
 	</tbody>
