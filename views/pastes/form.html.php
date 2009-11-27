66a49635b9db2ac899f706d363d0d7b1a2be28b1
alkemann <alek.morland@gmail.com>
2009-11-27 01:30:59 +0100

    updated views for `Paste` to take away modelname from input "names"

--- views/pastes/form.html.php
+++ views/pastes/form.html.php
@@ -10,22 +10,22 @@ $this->form->config(array('templates' => array('checkbox' =>
 
 
 if (isset($paste->id) && isset($paste->rev)) : ?>
-	<input type="hidden" name="Paste[id]" value="<?=$paste->id;?>" />
-	<input type="hidden" name="Paste[rev]" value="<?=$paste->rev;?>" />
+	<input type="hidden" name="id" value="<?=$paste->id;?>" />
+	<input type="hidden" name="rev" value="<?=$paste->rev;?>" />
 <?php endif;?>
 
 <?php
 	echo $this->form->label('Paste.author', 'Who are you?', array(
 		'class' => 'required'
 	));
-	echo $this->form->text('Paste[author]', array(
+	echo $this->form->text('author', array(
 		'id' => 'Paste.author',
 		'value' => $paste->author
 	));
 	if (isset($errors['author'])) {
 		echo '<p style="color:red">'.$errors['author'].'</p>';
 	}
-	echo $this->form->checkbox('Paste[remember]', array(
+	echo $this->form->checkbox('remember', array(
 		'id' => 'Paste.remember',
 		'checked' => 'checked',
 	));
@@ -36,7 +36,7 @@ if (isset($paste->id) && isset($paste->rev)) : ?>
 	echo $this->form->label('Paste.content', 'What do you have to say?', array(
 		'class' => 'required'
 	));
-	echo $this->form->textarea('Paste[content]', array(
+	echo $this->form->textarea('content', array(
 		'id' => 'Paste.content',
 		'rows' => '20',
 		'value' => $paste->content
@@ -49,7 +49,7 @@ if (isset($paste->id) && isset($paste->rev)) : ?>
 
 <?php
 	echo $this->form->label('Paste.language', 'language');
-	echo $this->form->select('Paste[language]', array_combine($languages, $languages), array(
+	echo $this->form->select('language', array_combine($languages, $languages), array(
 		'id' => 'Paste.language',
 		'value' => $paste->language
 	));
@@ -57,7 +57,7 @@ if (isset($paste->id) && isset($paste->rev)) : ?>
 		echo '<p style="color:red">'.$errors['language'].'</p>';
 	}
 
-	echo $this->form->checkbox('Paste[permanent]', array(
+	echo $this->form->checkbox('permanent', array(
 		'id' => 'Paste.permanent'
 	));
 	echo $this->form->label('Paste.permanent', " permanent");
