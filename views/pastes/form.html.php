46cee43747d5090a06d1af43a00740b09cfef9b1
alkemann <alek.morland@gmail.com>
2009-12-06 12:56:15 +0100

    updated form view 'name' values

--- views/pastes/form.html.php
+++ views/pastes/form.html.php
@@ -2,7 +2,7 @@
 
 	<div class="section paste-content">
 		<div class="input textarea">
-			<textarea name="Paste[content]" rows="25"><?=$paste->content;?></textarea>
+			<textarea name="content" rows="25"><?=$paste->content;?></textarea>
 		</div>
 		<?php echo (isset($paste->errors['content'])) ?
 			'<p class="error">'.$paste->errors['content'].'</p>' : null;
@@ -12,23 +12,23 @@
 	<div class="section paste-meta">
 
 		<?php if (isset($paste->id) && isset($paste->rev)) : ?>
-			<input type="hidden" name="Paste[id]" value="<?=$paste->id;?>" />
-			<input type="hidden" name="Paste[rev]" value="<?=$paste->rev;?>" />
+			<input type="hidden" name="id" value="<?=$paste->id;?>" />
+			<input type="hidden" name="rev" value="<?=$paste->rev;?>" />
 		<?php endif; ?>
-		<label for="Paste[author]">Name/Nick</label>
-		<input type="text" name="Paste[author]" value="<?=$paste->author;?>" />
+		<label for="Paste.author">Name/Nick</label>
+		<input type="text" name="author" id="Paste.author" value="<?=$paste->author;?>" />
 		<?php echo (isset($paste->errors['author'])) ?
 			'<p class="error">'.$paste->errors['author'].'</p>' : null;
 		?>
 		<div class="checkbox">
-		<input type="hidden" name="Paste[remember]" value="0" />
+		<input type="hidden" name="remember" value="0" />
 		<input type="checkbox" id="Paste.remember" value="1"
-			<?=($paste->remember) ? 'checked=checked' : null;?> name="Paste[remember]" /> &nbsp;
+			<?=($paste->remember) ? 'checked=checked' : null;?> name="remember" /> &nbsp;
 		<label for="Paste.remember">Remember me</label>
 		</div>
 
 		<label for="Paste.language">Language</label>
-		<select id="Paste.language" name="Paste[language]" value="">
+		<select id="Paste.language" name="language" value="">
 		<?php
 			foreach ($languages as $lang) {
 				if ($lang == $paste->language) {
@@ -43,12 +43,12 @@
 			'<p class="error">'.$paste->errors['language'].'</p>' : null;
 		?>
 		<div class="checkbox">
-		<input type="hidden" name="Paste[permanent]" value="0" />
+		<input type="hidden" name="permanent" value="0" />
 		<input type="checkbox" id="Paste.permanent" value="1"
-			<?=($paste->permanent) ? 'checked=checked' : null;?> name="Paste[permanent]" /> &nbsp;
+			<?=($paste->permanent) ? 'checked=checked' : null;?> name="permanent" /> &nbsp;
 		<label for="Paste.permanent">Save this paste</label>
 		</div>
-		<input type="submit" value="Paste this" />
+		<?php echo $this->form->submit('Paste this');?>
 	</div>
 	<div class="notice">
 		<small>Pastes are publicly viewable. Paste wisely.</small>
