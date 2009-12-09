29c79671509a1ab9849762a7ec3bac583654a131
alkemann <alek.morland@gmail.com>
2009-12-09 18:38:23 +0100

    displaying multiple validation error messages

--- views/pastes/form.html.php
+++ views/pastes/form.html.php
@@ -16,7 +16,7 @@ if (isset($paste->id) && isset($paste->rev)) {
 echo $this->form->label('Paste.author', 'Who are you?', array('class' => 'required'));
 echo $this->form->text('author', array('id' => 'Paste.author'));
 if (isset($errors['author'])) {
-	echo '<p style="color:red">'.$errors['author'].'</p>';
+	echo '<p style="color:red">'.implode($errors['author'], "<br>").'</p>';
 }
 
 echo $this->form->checkbox('remember', array('id' => 'Paste.remember'));
@@ -28,7 +28,7 @@ echo $this->form->label('Paste.remember', ' remember');
 echo $this->form->label('Paste.content', 'Paste content', array('class' => 'required'));
 echo $this->form->textarea('content', array('id' => 'Paste.content', 'rows' => '20'));
 if (isset($errors['content'])) {
-	echo '<p style="color:red">'.$errors['content'].'</p>';
+	echo '<p style="color:red">'.implode($errors['content'], "<br>").'</p>';
 }
 
 ?>
@@ -40,7 +40,7 @@ echo $this->form->select('language', array_combine($languages, $languages), arra
 	'id' => 'Paste.language'
 ));
 if (isset($errors['language'])) {
-	echo '<p style="color:red">'.$errors['language'].'</p>';
+	echo '<p style="color:red">'.implode($errors['language'], "<br>").'</p>';
 }
 
 echo $this->form->checkbox('permanent', array('id' => 'Paste.permanent'));
