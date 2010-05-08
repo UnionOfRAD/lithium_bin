c62f8eefa74cc6a464116558765a97cfab90c903
alkemann <alek.morland@gmail.com>
2010-05-08 13:46:48 +0200

    added a modified text to view

--- views/pastes/view.html.php
+++ views/pastes/view.html.php
@@ -1,6 +1,8 @@
 <div class="view">
 	<h2><?=$paste->preview;?></h2>
-<?php if (!$paste->author || $paste->author == '') { ?>
+<?php
+if ($paste->created == $paste->modified) {
+	if (!$paste->author || $paste->author == '') { ?>
 	<h3>This <strong><?=$paste->language;?></strong> paste was created
 		<strong>anonymously</strong>
 		at <strong><?=$paste->created;?></strong></h3>
@@ -8,7 +10,20 @@
 	<h3>This <strong><?=$paste->language;?></strong> paste was created by
 		<strong><?=$paste->author;?></strong> at
 		<strong><?=$paste->created;?></strong></h3>
-<?php } ?>
+<?php }
+} else {
+	if (!$paste->author || $paste->author == '') { ?>
+	<h3>This <strong><?=$paste->language;?></strong> paste was last modified
+		<strong>anonymously</strong>
+		at <strong><?=$paste->modified;?></strong></h3>
+<?php } else  { ?>
+	<h3>This <strong><?=$paste->language;?></strong> paste was last modified by
+		<strong><?=$paste->author;?></strong> at
+		<strong><?=$paste->modified;?></strong></h3>
+<?php
+	}
+}
+?>
 	<p>
 		<a href="#" id="toggle">clean</a> &middot;
 		<a href="#">copy</a> &middot;
