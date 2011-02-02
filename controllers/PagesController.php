2b07c4b8edef07a3e2814b41cb596c7b1100cd71
David Persson <davidpersson@gmx.de>
2011-02-02 19:19:07 +0100

    Refactoring PagesController.

--- controllers/PagesController.php
+++ controllers/PagesController.php
@@ -2,28 +2,15 @@
 
 namespace app\controllers;
 
-use lithium\util\Inflector;
-
 class PagesController extends \lithium\action\Controller {
-
-	public $helpers = array('Html');
 
 	public function view() {
 		$path = func_get_args();
 
-		if (!count($path)) {
+		if (empty($path)) {
 			$path = array('home');
 		}
-
-		$count = count($path);
-		$page = $subpage = $title = null;
-
-		$page = (!empty($path[0]) ? $path[0] : $page);
-		$subpage = (!empty($path[1]) ? $path[1] : $subpage);
-		$title = (!empty($path[$count - 1]) ? Inflector::humanize($path[$count - 1]) : $title);
-
-		$this->set(compact('page', 'subpage', 'title'));
-		$this->render(join('/', $path));
+		$this->render(array('template' => join('/', $path)));
 	}
 }
 
