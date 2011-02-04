c060e3585f0a27a325e075d9c8c820d29e8acd24
David Persson <davidpersson@gmx.de>
2011-02-04 21:53:45 +0100

    Rewriting view template.

--- views/pastes/view.html.php
+++ views/pastes/view.html.php
@@ -1,25 +1,51 @@
-<div class="view">
-	<h2><?=$paste->preview;?></h2>
-	<h3>This <strong><?=$paste->language;?></strong> paste was created by
-		<strong><?=$paste->author;?></strong> at
-		<strong><?=$paste->created;?></strong>
+<div class="paste">
+	<h2 class="preview"><?=$paste->preview;?></h2>
+	<h3>This <span class="language"><?=$paste->language;?></span> paste was created by
+		<span class="author user"><?=$paste->author;?></span> at
+		<span class="created"><?=$paste->created;?></span>
 	</h3>
-	<p>
-		<a href="#" id="toggle">clean</a> &middot;
-		<a href="#">copy</a> &middot;
-		<?php
-		if (isset($paste->password) && !empty($paste->password)) {
-
-			echo $this->html->link('edit', array(
-				'controller' => 'pastes', 'action' => 'edit', 'args' => array($paste->id)
-			));
-			echo ' &middot; <small>Password protected</small>';
-		} else
-			echo $this->html->link('edit', array(
-				'controller' => 'pastes', 'action' => 'edit', 'args' => array($paste->id)
-			));
-		?>
-	</p>
-	<div id="clean" ><pre><code><?=$paste->content;?></code></pre></div>
-	<div id="paste"><?php echo $paste->parsed;?></div>
+	<div class="nav">
+		<ul>
+			<li>
+				<?=$this->html->link('Color', '#', array(
+					'id' => 'toggle-color',
+					'title' => 'Toggle Color.'
+				)); ?>
+			</li>
+			<li>
+				<?=$this->html->link('Larger', '#', array(
+					'id' => 'code-bigger',
+					'title' => "Increases the code's font size."
+				)); ?>
+			</li>
+			<li>
+				<?=$this->html->link('Smaller', '#', array(
+					'id' => 'code-smaller',
+					'title' => "Decreases the code's font size."
+				)); ?>
+			</li>
+			<li>
+				<?=$this->html->link('Copy', '#', array(
+					'id' => 'copy-to-clipboard',
+					'title' => 'Copy to clipboard.'
+				)); ?>
+			</li>
+			<li>
+				<?=$this->html->link('Edit', array(
+					'controller' => 'pastes', 'action' => 'edit', 'args' => array($paste->id)
+				), array(
+					'id' => 'code-edit',
+					'title' => 'Edit this paste.'
+				)); ?>
+			</li>
+			<li>
+				<?=$this->html->link('Wrap Text', '#', array(
+					'id' => 'textwrap',
+					'title' => 'Wraps text in <pre> blocks.'
+				)); ?>
+			</li>
+		</ul>
+	</div>
+	<div class="raw"><pre><code><?=$paste->content;?></code></pre></div>
+	<div class="parsed"><?php echo $paste->parsed;?></div>
 </div>
