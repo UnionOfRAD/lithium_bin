403c9427e37d79106e59f11fbfe369772ce3b25e
David Persson <davidpersson@gmx.de>
2011-02-04 21:53:01 +0100

    Removing extra lines in routes definition.

--- config/routes.php
+++ config/routes.php
@@ -9,16 +9,11 @@
 use lithium\net\http\Router;
 
 Router::connect('/', array('controller' => 'pastes', 'action' => 'add'));
-
 Router::connect('/add', array('controller' => 'pastes', 'action' => 'add'));
 Router::connect('/add/{:args}', array('controller' => 'pastes', 'action' => 'add'));
-
 Router::connect('/edit/{:args}', array('controller' => 'pastes', 'action' => 'edit'));
-
 Router::connect('/view/{:args}', array('controller' => 'pastes', 'action' => 'view'));
-
 Router::connect('/latest', array('controller' => 'pastes', 'action' => 'index'));
-
 Router::connect('/{:controller}/{:action}/{:args}');
 
 ?>
