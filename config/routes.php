5d533988a063cec81221ab6b9f8729ec4dd3baf4
David Persson <davidpersson@gmx.de>
2011-02-04 21:32:05 +0100

    Adding pastes view route.

--- config/routes.php
+++ config/routes.php
@@ -15,6 +15,8 @@ Router::connect('/add/{:args}', array('controller' => 'pastes', 'action' => 'add
 
 Router::connect('/edit/{:args}', array('controller' => 'pastes', 'action' => 'edit'));
 
+Router::connect('/view/{:args}', array('controller' => 'pastes', 'action' => 'view'));
+
 Router::connect('/latest', array('controller' => 'pastes', 'action' => 'index'));
 Router::connect('/latest/page:{:page:[0-9]+}', array(
 	'controller' => 'pastes', 'action' => 'index', 'page' => 1
