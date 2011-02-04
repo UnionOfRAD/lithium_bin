72923b911f3d4352de567ba70681628faef555dd
David Persson <davidpersson@gmx.de>
2011-02-04 21:13:17 +0100

    Updating paste form with notice and layout from u1m.

--- views/pastes/form.html.php
+++ views/pastes/form.html.php
@@ -1,38 +1,45 @@
-<?=$this->form->create($paste, array('url' => $url, 'method' => 'POST')); ?>
+<?=$this->form->create($paste, compact('url') + array('method' => 'POST')); ?>
 
 <?php if (isset($paste->id) && isset($paste->rev)): ?>
 	<?=$this->form->hidden('id'); ?>
 	<?=$this->form->hidden('rev'); ?>
 <?php endif; ?>
-<?=$this->form->field('author', array(
-	'label' => 'Author'
-)); ?>
-<?=$this->form->field('remember', array(
-	'label' => 'remember me',
-	'type' => 'checkbox'
-)); ?>
-<?=$this->form->field('content', array(
-	'type' => 'textarea'
-)); ?>
-<?=$this->form->field('language', array(
-	'type' => 'select',
-	'list' => array_combine($languages, $languages)
-)); ?>
-<?=$this->form->field('permanent', array(
-	'type' => 'checkbox',
-	'label' => 'permanent (shows up in index)'
-)); ?>
 
-<?php if ((isset($paste->id) && isset($paste->password) && !empty($paste->password)) || !isset($paste->id)) : ?>
-	<?=$this->form->field('password', array(
-		'type' => 'password',
-		'label' => 'Password (provide one to lock or leave empty if not)'
+<div class="section paste-content">
+	<?=$this->form->field('content', array(
+		'type' => 'textarea'
 	)); ?>
-<?php endif; ?>
+</div>
+<div class="section paste-meta">
+	<?=$this->form->field('author', array(
+		'label' => 'Author'
+	)); ?>
+	<?=$this->form->field('remember', array(
+		'label' => 'remember me',
+		'type' => 'checkbox'
+	)); ?>
+	<?=$this->form->field('language', array(
+		'type' => 'select',
+		'list' => array_combine($languages, $languages)
+	)); ?>
+	<?=$this->form->field('permanent', array(
+		'type' => 'checkbox',
+		'label' => 'permanent (shows up in index)'
+	)); ?>
+	<?php if ((isset($paste->id) && isset($paste->password) && !empty($paste->password)) || !isset($paste->id)) : ?>
+		<?=$this->form->field('password', array(
+			'type' => 'password',
+			'label' => 'Password (provide one to lock or leave empty if not)'
+		)); ?>
+	<?php endif; ?>
+</div>
 <?php if (isset($paste->id)): ?>
 	<?=$this->form->submit('save') ?>
 	<?=$this->form->submit('save copy', array('name' => 'copy')); ?>
 <?php else: ?>
 	<?=$this->form->submit('create') ?>
 <?php endif; ?>
-</form>
+<div class="notice">
+	<small>Pastes are publicly viewable. Paste wisely.</small>
+</div>
+<?=$this->form->end(); ?>
